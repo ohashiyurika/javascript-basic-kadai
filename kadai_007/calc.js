ba3c82731fb8794aad9a6710bed1424e70b52e93
ohashiyurika <hasyuri38@gmail.com>
2025-02-21 16:02:44 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,16 +1,14 @@
 let num = 60;
 
-switch (num % 15) {
-  case 0:
-    console.log('3と5の倍数です');
-    break;
-  case 5 || 10:
-    console.log('5の倍数です');
-    break;
-  case 3 || 6 || 9 || 12:
-    console.log('3の倍数です');
-    break;
-  default:
+if (num % 15 === 0) {
+  console.log('3と5の倍数です');
+}
+else if (num % 5 === 0) {
+  console.log('5の倍数です');
+}
+else if (num % 3 === 0) {
+  console.log('3の倍数です');
+}
+else {
   console.log(num);
-  break;
 }
